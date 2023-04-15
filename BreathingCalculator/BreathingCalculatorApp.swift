cd93463a7d8f5ac6031f830e0d65ad54710d2984
Semih Karahan <karahansemih@hotmail.com>
2023-04-16 00:24:01 +0300

    Initial Commit

---
+++ BreathingCalculator/BreathingCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathingCalculatorApp.swift
+//  BreathingCalculator
+//
+//  Created by Semih Karahan on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BreathingCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
